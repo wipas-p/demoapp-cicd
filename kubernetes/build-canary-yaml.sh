bae8d01094dabf28bf85c5efc30737e0c22e770d
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2018-11-09 17:36:29 +0700

    Create build-canary-yaml.sh

---
+++ kubernetes/build-canary-yaml.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+cat <<EOF> deployapp-canary.yaml
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: demoapp-canary
+  labels:
+    app: demoapp-canary
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: demoapp
+  template:
+    metadata:
+      labels:
+        app: demoapp
+        track: canary
+    spec:
+      containers:
+      - name: demoapp
+        image: $IMAGE_NAME:$BUILD_NUMBER
+        ports:
+        - containerPort: 8080
+EOF
